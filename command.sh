1f2ee931d6e61e7768ad932cc72e1e04aa044899
Ronak Sharma <sharmaronak79@yahoo.com>
2019-09-08 20:57:57 -0400

    command.sh

---
+++ command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "hello $USER How are you today?"
+echo "your host machine is $(hostname)"
+echo ""
+echo "you are working in this directory : $(pwd)"
+echo ""
+echo "Today's date is :"
+echo $(date)
+
+echo "this month's calander is :"
+echo "$(cal)"
