b8150e94c97fffea3c2b116845fbc84cb1f014ca
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-05-11 16:45:09 -0400

    Update internet.sh

--- internet.sh
+++ internet.sh
@@ -5,4 +5,4 @@ sudo iptables --table nat --append POSTROUTING --out-interface wlo1 -j MASQUERAD
 sudo iptables --append FORWARD --in-interface enx7804734d3ee8 -j ACCEPT
 sudo echo 1 > /proc/sys/net/ipv4/ip_forward
 sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-echo "all good"
+echo "all_good"
