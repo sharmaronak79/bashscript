a3f9b66e513adc5d5f65a6ac158c1982666a60ca
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-10-13 22:33:50 -0400

    Update command.sh

--- command.sh
+++ command.sh
@@ -5,7 +5,7 @@ echo "your host machine is $(hostname)"
 echo ""
 echo "you are working in this directory : $(pwd)"
 echo ""
-echo "Today's date is :"
+echo "Today's date is:"
 echo $(date)
 
 echo "this month's calander is :"
